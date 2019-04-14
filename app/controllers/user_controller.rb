0b2064a38aba0a4afbf2c41b1ac1b853762f79b6
Ahmed <ahmahm@hotmail.co.jp>
2019-04-14 17:02:48 +0200

    test icon

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,28 +1,23 @@
 class UserController < ApplicationController
 
 	def new
-
 		message = User.new(name: params[:name], email: params[:email], message: params[:message])
 		message.save
-
 		respond_to do |format|
 			format.html { redirect_to request.referrer}
 		end	
 	end
 
 	def admin
-
 		if params[:password] == "Superman08@"
 			respond_to do |format|
 				format.html { redirect_to message_path}
 			end	
-		
 		end
 	end
 
 	def index
 		@users = User.all
-
 	end
 
 end
