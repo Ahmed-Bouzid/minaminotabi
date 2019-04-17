1a07cd57db103d13e425fa269e77d9324d566871
Ahmed <ahmahm@hotmail.co.jp>
2019-04-17 22:28:43 +0200

    mailer update

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -6,7 +6,7 @@ class Mailer < ApplicationMailer
     @user_mail = user_mail
     @message = message
 
-    mail(to: @user_mail, subject: '南の旅の予約', body:'Votre message a bien ete recu.')
+    mail(to: @user_mail, subject: '南の旅の予約', body:"Bonjour #{@user_name}, votre message a bien ete recu par Minami no Tabi. Une reponse vous sera rendue au plus vite !")
 
   end
 
@@ -16,7 +16,7 @@ class Mailer < ApplicationMailer
     @user_mail = user_mail
     @message = message
 
-    mail(to: "minaminotabi@gmail.com", subject: 'New Message', from: @user_mail,  body:"#{@user_name} just sent you un message : '#{@message}' ")
+    mail(to: "minaminotabi@gmail.com", subject: 'New Message', from: @user_mail,  body:"#{@user_name}, [#{@user_mail}] just sent you un message : '#{@message}' ")
 
   end
 
