fada59040138a67e48d372de961e9bc7a949399a
Ahmed <ahmahm@hotmail.co.jp>
2019-04-17 21:39:14 +0200

    mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -5,6 +5,9 @@ class Mailer < ApplicationMailer
       @user_mail = user_mail
       @message = message
       @admin_mail = minaminotabi@gmail.com
+      mail(to: '@user_name', subject: '南の旅の予約', body:'Votre message a bien ete recu.')
+      mail(to: 'minaminotabi@gmail.com', subject: '南の旅の予約', body:'#{@message}')
+
     end
 
 
